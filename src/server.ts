11e6272491c979a2179fcf440bff5759c0e6908a
theaymanian2803 <sberechou@gmail.com>
2026-09-13 17:05:01 +0100

    feat: expose Cloudflare env bindings to server modules

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "./lib/error-capture";
 
 import { consumeLastCapturedError } from "./lib/error-capture";
 import { renderErrorPage } from "./lib/error-page";
+import { setCloudflareEnv } from "./server/env";
 
 type ServerEntry = {
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
@@ -46,6 +47,7 @@ function isH3SwallowedErrorBody(body: string): boolean {
 
 export default {
   async fetch(request: Request, env: unknown, ctx: unknown) {
+    setCloudflareEnv(env);
     try {
       const handler = await getServerEntry();
       const response = await handler.fetch(request, env, ctx);
